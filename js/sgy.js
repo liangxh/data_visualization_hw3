e21298777587c38858006d9f6fee79c5c5dd3a6a
Guangyao Shen <shengy@rd.netease.com>
2017-05-29 22:17:58 +0800

    试一下 push

--- js/sgy.js
+++ js/sgy.js
@@ -3,7 +3,17 @@ function show_userbehaviour_userreturning(){
     $("#explore-container").html("<h2>HELLO WORLD to USERRETURNING</h2>");
 }
 
+$("#left-nav-userbehaviour #left-nav-sessionnum").click(function(){
+    reset_active_nav();
+    //$("#nav-userbehaviour").addClass("active");
+    $("#left-nav-userbehaviour").show();
+    activate_left_nav("sessionnum");
 
+    var data = Array(); //把准备好的數據直接放在这里或js的其他地方
+
+    var html="HELLO WORLD";
+    $("#explore-container").html(html);
+});
 /* TODO
 this is the EMPLATE for new entry page
     在index.html中<ul id="left-nav-behaviour">下参考现有的<li>添加自己的子页
